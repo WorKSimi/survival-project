edf80a98eed143921a125a576ebe033262c40e31
WorKSimi <116308823+WorKSimi@users.noreply.github.com>
2022-11-02 15:12:32 -0400

    Part 3 of Inventory Tutorial complete
    Everything works as it should, part 3 of the tutorial is complete

--- survival-project/Assets/Scripts/UI Scripts/StaticInventoryDisplay.cs
+++ survival-project/Assets/Scripts/UI Scripts/StaticInventoryDisplay.cs
@@ -7,7 +7,7 @@ public class StaticInventoryDisplay : InventoryDisplay
     [SerializeField] private InventoryHolder inventoryHolder;
     [SerializeField] private InventorySlot_UI[] slots;
 
-    protected override void Start()
+    public override void Start()
     {
         base.Start();
 
