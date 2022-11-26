20bc558b3dadbd8df22a73d87d131a64915005ab
WorKSimi <116308823+WorKSimi@users.noreply.github.com>
2022-11-26 12:38:01 -0500

    More Crafting
    Start on other recipes

---
+++ survival-project/Assets/Scripts/CraftingScripts/AnywhereCrafting.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class AnywhereCrafting : MonoBehaviour
+{
+    [SerializeField] private GameObject anywhereCraftingMenu;
+    [SerializeField] private GameObject thisPlayer;
+
+    private void Awake()
+    {
+        anywhereCraftingMenu.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            anywhereCraftingMenu.SetActive(false);
+        }
+        else if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            anywhereCraftingMenu.SetActive(true);
+        }
+    }
+
+    public void CraftCraftingTable()
+    {
+        Debug.Log("Trying to Craft Crafting Table");
+        var playerInventoryHolder = thisPlayer.GetComponent<PlayerInventoryHolder>();
+
+        var woodComponent = new CraftRecipeItem
+        {
+            displayName = "wood",
+            quantity = 5
+        };
+
+        var components = new List<CraftRecipeItem>() { woodComponent };
+        {
+            playerInventoryHolder.inventorySystem.CraftItem(components);
+            Debug.Log("Contains Wood");
+        }
+    }
+}
