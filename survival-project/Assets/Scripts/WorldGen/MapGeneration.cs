12bd30ed95240c5b043d605c7d69ce7a1f783418
WorKSimi <116308823+WorKSimi@users.noreply.github.com>
2022-12-31 16:01:05 -0500

    Work on Worldgen

--- survival-project/Assets/Scripts/WorldGen/MapGeneration.cs
+++ survival-project/Assets/Scripts/WorldGen/MapGeneration.cs
@@ -10,12 +10,11 @@ public class MapGeneration : MonoBehaviour
     public RuleTile sandTile;
     public RuleTile grassTile;
     public RuleTile wallTile;
+    public RuleTile dirtTile;
+    public RuleTile stoneTile;
 
-    public GameObject testTile0;
-    public GameObject testTile1;
-    public GameObject testTile2;
-    public GameObject testTile3;
     public GameObject tree;
+    public GameObject caveEntrance;
 
     [Header("Dimensions")]
     public int width = 50;
@@ -33,14 +32,13 @@ public class MapGeneration : MonoBehaviour
     private Tilemap wallTilemap;
     private Tilemap groundTilemap;
     private Tilemap carpetTilemap;
-
+    //private GameObject[] caveEntrances; //Creates an array to store cave entrances
+    //private int currentCaveEntrances = 0;
 
     // ground, carpet, wall, interactive
-
     // Start is called before the first frame update
     void Start()
     {
-        //Instantiate(worldGrid, new Vector3(0, 0, 0), Quaternion.identity);
         GetTilemaps();
         GenerateMap();
     }
@@ -62,36 +60,57 @@ public class MapGeneration : MonoBehaviour
         heightWaves[1].seed = Random.Range(1.0f, 100.0f);
 
         heightMap = NoiseGenerator.Generate(width, height, scale, heightWaves, offset); //height map
-        for (int x=0; x<width; ++x)
+        for (int x = 0; x < width; ++x)
         {
-            for (int y=0; y<height; ++y)
+            for (int y = 0; y < height; ++y) //Cycle through the noise map
             {
                 //Instantiate tile on tilemap based on height value               
                 var height = heightMap[x, y];
 
                 if (height < 0.2f) //Water
                 {
-                    groundTilemap.SetTile(new Vector3Int(x, y, 0), waterTile);                   
+                    groundTilemap.SetTile(new Vector3Int(x, y, 0), waterTile);
                 }
-                else if (height < 0.3f) //Sand
+                else if (height < 0.25f) //Sand
                 {
                     groundTilemap.SetTile(new Vector3Int(x, y, 0), sandTile);
                 }
-                else if (height < 0.7f) //Grass 
+                else if (height < 0.6f) //Grass 
                 {
                     groundTilemap.SetTile(new Vector3Int(x, y, 0), grassTile);
 
-                    if (Random.value >= 0.9) //10 percent chance
+                    if (Random.value >= 0.96) //4 percent chance
                     {
-                        Instantiate(tree, new Vector3(x, y, -0.1f), Quaternion.identity);
+                        Instantiate(tree, new Vector3Int(x, y, 0), Quaternion.identity);
                     }
                 }
-                else if (height <= 1.0f) //Dirt Wall
+                else if (height < 0.7f) //Dirt Ground
                 {
-                    groundTilemap.SetTile(new Vector3Int(x, y, 0), wallTile);
+                    groundTilemap.SetTile(new Vector3Int(x, y, 0), dirtTile);
+                    if (Random.value >= 0.95) //5 percent chance
+                    {
+                        Instantiate(caveEntrance, new Vector3(x, y, 0), Quaternion.identity);
+                    }
                 }
-
+                else if (height < 1.0f) //Dirt Wall
+                {
+                    wallTilemap.SetTile(new Vector3Int(x, y, 0), wallTile);
+                }
             }
         }
     }
 }
+    //void SpawnCaveEntrance(int x, int y)
+    //{
+    //    int maxEntrnaces = 5;
+
+    //    if (currentCaveEntrances < maxEntrnaces)
+    //    {
+    //       Instantiate(caveEntrance, new Vector3(x, y, 0), Quaternion.identity);
+    //       currentCaveEntrances++;
+    //    }
+    //    else if (currentCaveEntrances >= maxEntrnaces)
+    //    {
+    //       Debug.Log("Cave Limit Reached");
+    //    }
+    //}
